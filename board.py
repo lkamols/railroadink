a2965f9ee77987b7ccc925289652d91c348c4a2d
Luke Kamols <lkamols4@gmail.com>
2021-02-16 12:18:20 +1000

    refactor of sides and added start tiles

--- board.py
+++ board.py
@@ -11,6 +11,8 @@ LEFT_OFFSET = 30
 BOARD_WIDTH = 669
 BOARD_HEIGHT = 669
 
+
+
 PHOTOS_FOLDER = "Railroad-Pictures/"
 
 class Edge(Enum):
@@ -37,6 +39,9 @@ class Piece(Enum):
     RAILWAY_JUNCTION = 12
     CORNER_JUNCTION = 13
     CROSS_JUNCTION = 14
+    #start pieces (the edge tiles)
+    START_RAILWAY = 15
+    START_HIGHWAY = 16
     
 class Orientation(Enum):
     DEFAULT = 0
@@ -44,6 +49,20 @@ class Orientation(Enum):
     TURN_90 = 1
     TURN_180 = 2
     TURN_270 = 3
+    
+class Side(Enum):
+    TOP = 0
+    RIGHT = 1
+    BOTTOM = 2
+    LEFT = 3
+    
+#start locations of all the highways and railways
+highway_start_positions = [(-1, 1, Orientation.TURN_180), (-1, 5, Orientation.TURN_180),
+                           (3, -1, Orientation.TURN_90), (3, NUM_COLS, Orientation.TURN_270),
+                           (NUM_ROWS, 1, Orientation.DEFAULT), (NUM_ROWS, 5, Orientation.DEFAULT)]
+railway_start_positions = [(-1, 3, Orientation.TURN_180), (NUM_ROWS, 3, Orientation.DEFAULT),
+                           (1, -1, Orientation.TURN_90), (5, -1, Orientation.TURN_90),
+                           (1, NUM_COLS, Orientation.TURN_270), (5, NUM_COLS, Orientation.TURN_270)]
 
 class Tile:
     
@@ -63,7 +82,9 @@ class Tile:
         Piece.HIGHWAY_JUNCTION: (Edge.H, Edge.H, Edge.H, Edge.H, False),
         Piece.RAILWAY_JUNCTION: (Edge.R, Edge.R, Edge.R, Edge.R, False),
         Piece.CORNER_JUNCTION: (Edge.H, Edge.R, Edge.R, Edge.H, False),
-        Piece.CROSS_JUNCTION: (Edge.H, Edge.R, Edge.H, Edge.R, False),         
+        Piece.CROSS_JUNCTION: (Edge.H, Edge.R, Edge.H, Edge.R, False),  
+        Piece.START_RAILWAY: (Edge.R, Edge.B, Edge.B, Edge.B, False),
+        Piece.START_HIGHWAY: (Edge.H, Edge.B, Edge.B, Edge.B, False)
         }
     
     tile_pics = {
@@ -82,7 +103,7 @@ class Tile:
         Piece.RAILWAY_JUNCTION: "railway-junction",
         Piece.CORNER_JUNCTION: "corner-junction",
         Piece.CROSS_JUNCTION: "cross-junction",             
-            }
+    }
     
     """
     Constructor
@@ -92,7 +113,9 @@ class Tile:
         self.orientation = orientation
         #load in all the left, right, down, up and overpass values
         #load these upon creation because they will be accessed multiple times
-        self.up, self.right, self.down, self.left, self.overpass = self.tile_map[piece]
+        self._dirs = {}
+        self._dirs[Side.TOP], self._dirs[Side.RIGHT], self._dirs[Side.BOTTOM], \
+                self._dirs[Side.LEFT], self.overpass = self.tile_map[piece]
         self.rotate(orientation)
         
     """
@@ -102,7 +125,8 @@ class Tile:
         #rotate to the given orientation, enum values are the number of rotations required
         for rotations in range(orientation.value):
             #change all the assignments in the same line, handles the required temporary variables
-            self.up, self.right, self.down, self.left = self.left, self.up, self.right, self.down
+            self._dirs[Side.TOP], self._dirs[Side.RIGHT], self._dirs[Side.BOTTOM], self._dirs[Side.LEFT] = \
+                    self._dirs[Side.LEFT], self._dirs[Side.TOP], self._dirs[Side.RIGHT], self._dirs[Side.BOTTOM]
          
     """
     get the image associated with this piece and orientation, scaled to fit on the board image
@@ -122,6 +146,12 @@ class Tile:
         return piece_image
     
     """
+    get the edge type associated with one of the directions
+    """
+    def get_edge_type_on_side(self, side):
+        return self._dirs[side]
+    
+    """
     override the equality function to treat two Tiles with the same value the same
     """
     def __eq__(self, obj):
@@ -134,17 +164,38 @@ class Tile:
     override the representation function so printing is readable
     """
     def __repr__(self):
-        return "({0},{1},{2},{3},{4},{5},{6})".format(self.piece.name, self.orientation.name, self.up.name, self.right.name, self.down.name, self.left.name, "T" if self.overpass else "F")
+        return "({0},{1},{2},{3},{4},{5},{6})".format(self.piece.name, self.orientation.name, 
+               self._dirs[Side.TOP].name, self._dirs[Side.RIGHT].name, self._dirs[Side.BOTTOM].name,
+               self._dirs[Side.LEFT].name, "T" if self.overpass else "F")
     
 class Board:
     
     def __init__(self):
         #create an empty board with no tiles in it yet
         self.board = [[None]*NUM_COLS for i in range(NUM_ROWS)]
-        
+        self.initialise_start_tiles()
+      
+    """
+    add a tile to the board
+    """
     def add_tile(self, row, col, piece, orientation):
         self.board[row][col] = Tile(piece, orientation)
-    
+        
+    """
+    add a start tile to the board, these are the pieces around the edge
+    """
+    def add_start_tile(self, row, col, piece, orientation):
+        self.start_pieces += [(row, col, Tile(piece, orientation))]
+        
+    """
+    initialise all the start pieces
+    """
+    def initialise_start_tiles(self):
+        self.start_pieces = []
+        for row, col, orientation in railway_start_positions:
+            self.add_start_tile(row, col, Piece.START_RAILWAY, orientation)
+        for row, col, orientation in highway_start_positions:
+            self.add_start_tile(row, col, Piece.START_HIGHWAY, orientation)
         
     """
     creates an image corresponding to the board,
@@ -167,44 +218,31 @@ class Board:
         else:
             im.save(file)
 
+   
+"""
+A cluster of adjoining pieces in the form of a disjoint set, 
+stores all the information about the cluster in the top level element in the disjoint set
+"""
+class Cluster:
     
-
+    """
+    initialise a cluster with just one tile at the given location
+    """
+    def __init__(self, row, col, tile):
+        self.rank = 1 #the max height of the disjoint cluster set
+        self.parent = None
+        self.tile = tile
+        self.row = row
+        self.col = col
+        self.blank_cluster = (tile == None) #whether this cluster is a blank cluster or a tiled cluster
+        
+    #def join
+            
        
 
 if __name__ == "__main__":
-    
-    print(Tile(Piece.CORNER_JUNCTION, Orientation.DEFAULT))
-    print(Tile(Piece.CORNER_JUNCTION, Orientation.TURN_90))
-    print(Tile(Piece.CORNER_JUNCTION, Orientation.TURN_180))
-    print(Tile(Piece.CORNER_JUNCTION, Orientation.TURN_270))
 
     board = Board()
     board.add_tile(2,3, Piece.RAILWAY_CORNER, Orientation.DEFAULT)
     board.fancy_board_print()
 
-
-    
-    
-    
-    
-    
-    
-"""
-    #create a dictionary with all the tiles, this is a constant across all boards, this is just data
-    tiles = {
-        Piece.RAILWAY_CORNER: generate_tile_rotations(Tile(Edge.R, Edge.B, Edge.B, Edge.R), 4),
-        Piece.RAILWAY_T: generate_tile_rotations(Tile(Edge.R, Edge.R, Edge.B, Edge.R), 4),
-        Piece.RAILWAY_STRAIGHT: generate_tile_rotations(Tile(Edge.R, Edge.B, Edge.R, Edge.B), 2),
-        Piece.HIGHWAY_CORNER: generate_tile_rotations(Tile(Edge.H, Edge.B, Edge.B, Edge.H), 4),
-        Piece.HIGHWAY_T: generate_tile_rotations(Tile(Edge.H, Edge.H, Edge.B, Edge.H), 4),
-        Piece.HIGHWAY_STRAIGHT: generate_tile_rotations(Tile(Edge.H, Edge.B, Edge.H, Edge.B), 2),
-        Piece.OVERPASS: generate_tile_rotations(Tile(Edge.H, Edge.R, Edge.H, Edge.R, overpass=True), 2),
-        Piece.STRAIGHT_STATION: generate_tile_rotations(Tile(Edge.R, Edge.B, Edge.H, Edge.B), 2),
-        Piece.CORNER_STATION: generate_tile_rotations(Tile(Edge.R, Edge.B, Edge.B, Edge.H), 4),
-        Piece.THREE_H_JUNCTION: generate_tile_rotations(Tile(Edge.H, Edge.H, Edge.R, Edge.H), 4),
-        Piece.THREE_R_JUNCTION: generate_tile_rotations(Tile(Edge.H, Edge.R, Edge.R, Edge.R), 4),
-        Piece.HIGHWAY_JUNCTION: generate_tile_rotations(Tile(Edge.H, Edge.H, Edge.H, Edge.H), 1),
-        Piece.RAILWAY_JUNCTION: generate_tile_rotations(Tile(Edge.R, Edge.R, Edge.R, Edge.R), 1),
-        Piece.CORNER_JUNCTION: generate_tile_rotations(Tile(Edge.H, Edge.R, Edge.R, Edge.H), 4),
-        Piece.CROSS_JUNCTION: generate_tile_rotations(Tile(Edge.H, Edge.R, Edge.H, Edge.R), 2)
-    }"""
