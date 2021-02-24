a3d8502509c8bc75ead421917128f4f479bae135
Luke Kamols <lkamols4@gmail.com>
2021-02-24 22:04:59 +1000

    slight update

--- attempt2/solver.py
+++ attempt2/solver.py
@@ -24,6 +24,7 @@ class LastMoveSolver:
         #sets
         S = {(i,j) for i in range(NUM_ROWS) for j in range(NUM_COLS)}
         T = Tile.get_all_variations()
+        E = [EdgeType.H, EdgeType.R]
         
         #x variables are for whether move m is made at square s
         X = {(t,s) : m.addVar(vtype=GRB.BINARY) for t in T for s in S}
@@ -31,6 +32,8 @@ class LastMoveSolver:
         Y = {(s,(s[0] + dr, s[1] + dc),e) : m.addVar(vtype=GRB.BINARY) for t in T 
              for s in S for (dr, dc) in [(0,1),(1,0)] for e in [EdgeType.H, EdgeType.R]
              if s[0] + dr < NUM_ROWS and s[1] + dc < NUM_COLS}
+        F = {((r,c),(r+dr,c+dc),e): m.addVar() for r in range(NUM_ROWS) for c in range(NUM_COLS) 
+                for (dr,dc) in [(0,1),(0,-1),(1,0),(-1,0)]}
         
         #constraints
         
@@ -39,28 +42,28 @@ class LastMoveSolver:
             m.addConstr(2 * Y[s, (s[0], s[1]+1), e] <= 
                               quicksum(X[t,s] for t in T if t.get_edge_type_on_side(Side.RIGHT) == e) +
                               quicksum(X[t,(s[0],s[1]+1)] for t in T if t.get_edge_type_on_side(Side.LEFT) == e))
-            for s in S if s[1] < NUM_COLS - 1 for e in [EdgeType.H, EdgeType.R]}
+            for s in S if s[1] < NUM_COLS - 1 for e in E}
         
         #constraints for if there are connections on any vertical edges
         vertical_y_constraints = {(s,e) :
             m.addConstr(2 * Y[s, (s[0]+1, s[1]), e] <=
                               quicksum(X[t,s] for t in T if t.get_edge_type_on_side(Side.BOTTOM) == e) +
                               quicksum(X[t,(s[0]+1,s[1])] for t in T if t.get_edge_type_on_side(Side.TOP) == e))
-            for s in S if s[0] < NUM_ROWS - 1 for e in [EdgeType.H, EdgeType.R]}
+            for s in S if s[0] < NUM_ROWS - 1 for e in E}
           
         #constraints for clashes on edges joining squares horizontally, preventing railways and highways being connected
         horizontal_clashes = {(s,e) :
             m.addConstr(quicksum(X[t,s] for t in T if t.get_edge_type_on_side(Side.RIGHT) == e) +
                         quicksum(X[t,(s[0],s[1]+1)] for t in T 
                             if t.get_edge_type_on_side(Side.LEFT) == Side.opposite(e)) <= 1)
-            for s in S if s[0] < NUM_COLS - 1 for e in [EdgeType.H, EdgeType.R]}
+            for s in S if s[0] < NUM_COLS - 1 for e in E}
             
         #constraints for clashes on edges joining squares vertically, preventing railways & highways connecting
         vertical_clashes = {(s,e) :
             m.addConstr(quicksum(X[t,s] for t in T if t.get_edge_type_on_side(Side.BOTTOM) == e) +
                         quicksum(X[t,(s[0]+1,s[1])] for t in T if t.get_edge_type_on_side(Side.TOP) == Side.opposite(e))
                         <= 1)
-            for s in S if s[1] < NUM_ROWS - 1 for e in [EdgeType.H, EdgeType.R]}
+            for s in S if s[1] < NUM_ROWS - 1 for e in E}
             
             
         #ensure no illegal pieces are placed on start edges
@@ -124,50 +127,7 @@ class LastMoveSolver:
             if X[x].x > 0.9:
                 print(x)
         
-        self._print_result(m, X, T, S)
-    
-    """
-    creates all the constraints relating to clashes on internal edges, adds constraints preventing railways and highways
-    running into each other
-    """
-    def _clashes_on_edge_constraints(self, m, T, X, D, internal_edges):
-        edge_constraints = {} #constraints preventing pieces being positioned that directly clash with each other
-        edge_error_constraints = {} #constraints for the 
-        for edge in internal_edges:
-            if edge.is_vertical():
-                sLeft = (edge.get_row(), edge.get_col() - 1)
-                sRight = (edge.get_row(), edge.get_col())
-                #there can only be either a highway on the left OR a railway on the right
-                edge_constraints[edge, "H"] = m.addConstr(quicksum(X[t,sLeft] for t in T if (t, sLeft) in X and t.get_edge_type_on_side(Side.RIGHT) == EdgeType.H) +
-                                quicksum(X[t, sRight] for t in T if (t, sRight) in X and t.get_edge_type_on_side(Side.LEFT) == EdgeType.R) <= 1)
-                #similarly there can only be a railway on the left OR a highway on the right
-                edge_constraints[edge, "R"] = m.addConstr(quicksum(X[t,sLeft] for t in T if (t, sLeft) in X and t.get_edge_type_on_side(Side.RIGHT) == EdgeType.R) +
-                                quicksum(X[t, sRight] for t in T if (t, sRight) in X and t.get_edge_type_on_side(Side.LEFT) == EdgeType.H) <= 1)
-                #now for the internal errors constraints
-                #something on the left, nothing on the right
-                edge_error_constraints[edge, 0] = m.addConstr(quicksum(X[t,sLeft] for t in T if (t, sLeft) in X and t.get_edge_type_on_side(Side.RIGHT) != EdgeType.B) -
-                                quicksum(X[t, sRight] for t in T if (t, sRight) in X and t.get_edge_type_on_side(Side.LEFT) != EdgeType.B) <= D[edge, 0])
-                #something on the right, nothing on the left
-                edge_error_constraints[edge, 1] = m.addConstr(quicksum(X[t, sRight] for t in T if (t, sRight) in X and t.get_edge_type_on_side(Side.LEFT) != EdgeType.B) - 
-                                      quicksum(X[t,sLeft] for t in T if (t, sLeft) in X and t.get_edge_type_on_side(Side.RIGHT) != EdgeType.B) <= D[edge, 1])
-            else: #horizontal
-                sTop = (edge.get_row() - 1, edge.get_col())
-                sBottom = (edge.get_row(), edge.get_col())
-                #there can only be either a highway on the top OR a railway on the bottom
-                edge_constraints[edge, "H"] = m.addConstr(quicksum(X[t,sTop] for t in T if (t, sTop) in X and t.get_edge_type_on_side(Side.BOTTOM) == EdgeType.H) +
-                                quicksum(X[t, sBottom] for t in T if (t, sBottom) in X and t.get_edge_type_on_side(Side.TOP) == EdgeType.R) <= 1)
-                #similarly, railway top OR highway bottom
-                edge_constraints[edge, "H"] = m.addConstr(quicksum(X[t,sTop] for t in T if (t, sTop) in X and t.get_edge_type_on_side(Side.BOTTOM) == EdgeType.R) +
-                                quicksum(X[t, sBottom] for t in T if (t, sBottom) in X and t.get_edge_type_on_side(Side.TOP) == EdgeType.H) <= 1)
-                #now for the internal errors constraints
-                #something on the top, nothing on the bottom
-                edge_error_constraints[edge, 0] = m.addConstr(quicksum(X[t,sTop] for t in T if (t, sTop) in X and t.get_edge_type_on_side(Side.BOTTOM) != EdgeType.B) -
-                                      quicksum(X[t, sBottom] for t in T if (t, sBottom) in X and t.get_edge_type_on_side(Side.TOP) != EdgeType.B) <= D[edge, 0])
-                #something on the bottom, nothing on the top
-                edge_error_constraints[edge, 1] = m.addConstr(quicksum(X[t, sBottom] for t in T if (t, sBottom) in X and t.get_edge_type_on_side(Side.TOP) != EdgeType.B) -
-                                      quicksum(X[t,sTop] for t in T if (t, sTop) in X and t.get_edge_type_on_side(Side.BOTTOM) != EdgeType.B) <= D[edge, 1])
-        return edge_constraints, edge_error_constraints
-    
+        self._print_result(m, X, T, S)    
     
     """
     print the board with the solution attached
